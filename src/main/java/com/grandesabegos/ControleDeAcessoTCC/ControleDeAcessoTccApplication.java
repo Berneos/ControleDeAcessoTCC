2b6a74598eeb3a7e35457e0094c25fea0d7787f3
Berneos <brenomartins880@gmail.com>
2025-03-12 14:56:16 -0300

    Commit Inicial

---
+++ src/main/java/com/grandesabegos/ControleDeAcessoTCC/ControleDeAcessoTccApplication.java
@@ -0,0 +1,13 @@
+package com.grandesabegos.ControleDeAcessoTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeAcessoTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeAcessoTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grandesabegos/ControleDeAcessoTCC/ControleDeAcessoTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandesabegos.ControleDeAcessoTCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeAcessoTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
